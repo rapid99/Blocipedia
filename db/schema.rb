784d0bdd64f95765cf870de77efc3833405b28f1
“Matt <matthyman03@gmail.com>
2017-01-26 17:45:25 -0700

    merge master

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120012113) do
 
+ActiveRecord::Schema.define(version: 20170120012113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170120012113) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
