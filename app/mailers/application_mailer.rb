7e2dc8e9eb9d912c2dc87cc682dcdaec34466f23
“Matt <matthyman03@gmail.com>
2017-01-26 17:13:34 -0700

    fixed error in application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: "blocipedia@bloc.io"
   layout 'mailer'
 
-  
-  mail(to: user.email, subject: "Confirm Sign Up to Blocipedia")
+
+  # mail(to: user.email, subject: "Confirm Sign Up to Blocipedia")
 end
