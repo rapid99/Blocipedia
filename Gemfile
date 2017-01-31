a82006f382359136fae9227e594ee6581e6f3ea0
“Matt <matthyman03@gmail.com>
2017-01-30 18:56:34 -0700

    added users to wikis and reseeded

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise'
 gem 'faker'
 gem 'stripe'
 gem 'redcarpet'
+gem 'pundit'
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-20.times do
-  Wiki.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph(3),
-    private: false,
-  )
-wiki = Wiki.all
-end
-
 5.times do
   User.create!(
   email: Faker::Internet.email,
   password: "helloworld",
   )
 user = User.all
+end
+
+20.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph(3),
+    private: false,
+    user: User.all.sample
+  )
+wiki = Wiki.all
 end
 
 puts "#{Wiki.count} wikis created!"
